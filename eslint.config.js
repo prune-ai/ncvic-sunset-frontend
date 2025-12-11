cab8b98fe11419c10a9b63a110d6bbc954877c9a
Linus Mixson <linus@prune.co>
2025-12-11 00:51:39 -0500

    Bumping versions and reconfiuguring ESLint
    Assisted-by: Composer-1

--- eslint.config.js
+++ eslint.config.js
@@ -3,21 +3,24 @@ import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
-import { defineConfig, globalIgnores } from "eslint/config";
 
-export default defineConfig([
-  globalIgnores(["dist"]),
+export default tseslint.config(
+  { ignores: ["dist"] },
   {
     files: ["**/*.{ts,tsx}"],
     extends: [
       js.configs.recommended,
-      tseslint.configs.recommended,
+      ...tseslint.configs.recommendedTypeChecked,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
     ],
     languageOptions: {
-      ecmaVersion: 2020,
+      ecmaVersion: "latest",
       globals: globals.browser,
+      parserOptions: {
+        project: ["./tsconfig.app.json", "./tsconfig.node.json"],
+        tsconfigRootDir: import.meta.dirname,
+      },
     },
   },
-]);
+);
