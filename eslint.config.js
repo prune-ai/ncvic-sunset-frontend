5c250b792d70160e1ccdc72e87cb10b65922915c
Linus Mixson <linus@prune.co>
2025-12-11 00:53:43 -0500

    Setting up Prettier
    Assisted-by: Composer-1

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
+import prettier from "eslint-config-prettier";
 
 export default tseslint.config(
   { ignores: ["dist"] },
@@ -13,6 +14,7 @@ export default tseslint.config(
       ...tseslint.configs.recommendedTypeChecked,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
+      prettier,
     ],
     languageOptions: {
       ecmaVersion: "latest",
